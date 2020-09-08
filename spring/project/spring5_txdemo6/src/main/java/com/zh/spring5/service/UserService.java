e3e0cc6dc563dd8ddef905c99490ec107fa6418b
cai-xiansheng <1459578794@qq.com>
2020-09-08 18:46:17 +0800

    transactional

--- spring/project/spring5_txdemo6/src/main/java/com/zh/spring5/service/UserService.java
+++ spring/project/spring5_txdemo6/src/main/java/com/zh/spring5/service/UserService.java
@@ -3,6 +3,7 @@ package com.zh.spring5.service;
 import com.zh.spring5.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author cai-xiansheng
@@ -10,15 +11,35 @@ import org.springframework.stereotype.Service;
  * @create 2020-09-08 13:20
  */
 @Service
+@Transactional()
 public class UserService {
 
     // 注入dao
     @Autowired
     private UserDao userDao;
 
+
     public void accountMoney() {
-        userDao.reduceMoney();
-        userDao.addMoney();
+
+        //try {
+            // 第一步，开启事务
+
+            // 第二步，进行业务操作
+
+            userDao.reduceMoney();
+
+            // 模拟异常
+            int i = 10 / 0;
+
+            userDao.addMoney();
+
+            // 第三步，没有发生异常，提交事务
+
+        //} catch (Exception e) {
+            // 第四步，如果出现了异常，事务回滚
+
+            //e.printStackTrace();
+        //}
     }
 
 }
--- spring/project/spring5_txdemo6/src/test/java/com/zh/spring/Test.java
+++ spring/project/spring5_txdemo6/src/test/java/com/zh/spring/Test.java
@@ -1,9 +1,24 @@
 package com.zh.spring;
 
+import com.zh.spring5.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.PlatformTransactionManager;
+
 /**
  * @author cai-xiansheng
  * @Description
  * @create 2020-09-08 13:19
  */
 public class Test {
+
+    @org.junit.Test
+    public void test() {
+
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.accountMoney();
+    }
+
 }
